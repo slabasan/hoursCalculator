1521324826718dcaf583d67187d5a70d5c6d9f52
Stephanie Labasan <labasan.stephanie@gmail.com>
2015-09-02 09:13:08 -0700

    Main file

---
+++ hours_worked.py
@@ -0,0 +1,26 @@
+import sys
+import datetime
+
+def main(startTime, endTime):
+	date_now = datetime.datetime.now()
+
+	startHour = int(startTime.split(":")[0])
+	startMinute = int(startTime.split(":")[1])
+	endHour = int(endTime.split(":")[0])
+	endMinute = int(endTime.split(":")[1])
+	
+	if 1 <= endHour <= 11:
+		endHour = endHour + 12
+
+	start_time = datetime.datetime(date_now.year, date_now.month, date_now.day, startHour, startMinute, 00)
+	end_time = datetime.datetime(date_now.year, date_now.month, date_now.day, endHour, endMinute, 00)
+
+	hours = abs(start_time - end_time).total_seconds() / 3600.0
+
+	print "Total: %.2f hours\n" % hours
+
+if __name__=="__main__":
+	if len(sys.argv) != 3:
+		print "Error: python hours_worked.py <start:time> <end:time>\n"
+	else:
+		main(sys.argv[1], sys.argv[2])
